36ad699ba593db9be3c421c38fe17bef8a843511
HBMSRobotics <rtpack99@gmail.com>
2017-12-02 11:38:50 -0500

    Cleanup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorState.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
+
+/**
+ * Created by todd on 11/16/17.
+ */
+
+public class ColorSensorState extends StateMachine.State {
+
+    public ColorSensorState(String name, JewelTailHardware jewel_hw, String next_b_, String next_r_) {
+        super(name);
+        jt_hw = jewel_hw;
+        next_b = next_b_;
+        next_r = next_r_;
+    }
+
+    @Override
+    public void enter() {
+        // Does nothing.
+
+
+    }
+
+    @Override
+    public void exit() {
+        // Does nothing.
+    }
+
+    @Override
+    public String update(double secs) {
+
+    }
+
+    private String next_b;
+    private String next_r;
+    private JewelTailHardware jt_hw;
+}
